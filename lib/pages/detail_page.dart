fb333ffbc8fc8c5c6c61ebdbb31d8dddde7ba14d
zaenalashter <Santrialit77@gmail.com>
2022-05-24 15:01:09 +0700

    Details Page - Main Facilities

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:bwa_cozy/theme.dart';
+import 'package:bwa_cozy/widgets/facility_item.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
@@ -52,10 +53,12 @@ class DetailPage extends StatelessWidget {
                     color: whiteColor,
                   ),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       SizedBox(
                         height: 30,
                       ),
+                      //NOTE:TITLE
                       Padding(
                         padding: EdgeInsets.symmetric(
                           horizontal: edge,
@@ -106,21 +109,21 @@ class DetailPage extends StatelessWidget {
                                   'images/icon_star.png',
                                   width: 20,
                                 ),
-                                 SizedBox(
+                                SizedBox(
                                   width: 2,
                                 ),
                                 Image.asset(
                                   'images/icon_star.png',
                                   width: 20,
                                 ),
-                                 SizedBox(
+                                SizedBox(
                                   width: 2,
                                 ),
                                 Image.asset(
                                   'images/icon_star.png',
                                   width: 20,
                                 ),
-                                 SizedBox(
+                                SizedBox(
                                   width: 2,
                                 ),
                                 Image.asset(
@@ -129,6 +132,47 @@ class DetailPage extends StatelessWidget {
                                   color: Colors.grey,
                                 ),
                               ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: MAIN FACILITIES
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Main Faclities',
+                          style: blackTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                              name: 'Kitchen',
+                              imageUrl: 'images/icon_kitchen.png',
+                              total: 2,
+                            ),
+                            FacilityItem(
+                              name : 'Bedroom',
+                              imageUrl: 'images/icon_bedroom.png',
+                              total: 3,
+                            ),
+                            FacilityItem(
+                              name: 'Big Lemari',
+                              imageUrl: 'images/icon_cupboard.png',
+                              total: 3,
                             ),
                           ],
                         ),
