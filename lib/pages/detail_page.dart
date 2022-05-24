60c2bed806a3f8960dfcbb1df7f7517863d91147
zaenalashter <Santrialit77@gmail.com>
2022-05-24 14:34:05 +0700

    Details Page - Header

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -18,9 +18,7 @@ class DetailPage extends StatelessWidget {
               fit: BoxFit.cover,
             ),
             Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: edge,
-                 vertical: 30),
+              padding: EdgeInsets.symmetric(horizontal: edge, vertical: 30),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -39,7 +37,107 @@ class DetailPage extends StatelessWidget {
                   ),
                 ],
               ),
-            )
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 328,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Kuretakeso Hott',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Text.rich(
+                                  TextSpan(
+                                    text: '\$52',
+                                    style: purpleTextStyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: ' / month',
+                                        style: greyTextStyle.copyWith(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'images/icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'images/icon_star.png',
+                                  width: 20,
+                                ),
+                                 SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'images/icon_star.png',
+                                  width: 20,
+                                ),
+                                 SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'images/icon_star.png',
+                                  width: 20,
+                                ),
+                                 SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'images/icon_star.png',
+                                  width: 20,
+                                  color: Colors.grey,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
